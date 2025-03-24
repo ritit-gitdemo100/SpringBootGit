13b74cd5c7a8b9ad8138aa21a26b2a0da7535532
Global Developer <ritit.programs@gmail.com>
2025-03-24 12:04:56 +0530

    Third upload

--- src/main/java/com/rit/controller/HomeController.java
+++ src/main/java/com/rit/controller/HomeController.java
@@ -12,17 +12,11 @@ public class HomeController {
 	public String home() {
 		return "Welcome Home";
 	}
+
 	@GetMapping("/page1")
 	public String page1() {
 		return "Welcome Page1";
 	}
-	@GetMapping("/page2")
-	public String page2() {
-		return "Welcome Page2";
-	}
-	@GetMapping("/page3")
-	public String page3() {
-		return "Welcome Page3";
-	}
+
 }
 
